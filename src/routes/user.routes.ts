3bcc4c8b6f3ecbf007481b048d8de4b8574fab9f
weijie <qoo77823@hotmail.com>
2025-12-06 15:26:30 +0800

    feat: add routes/user.routes.ts

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,2 +1,10 @@
-// import { Router } from "express";
-// import {  } from "../"
+import { Router } from "express";
+import { container } from "../di/container";
+
+const router = Router();
+const userController = container.userController;
+
+router.post("/register", userController.login);
+router.post("/login", userController.login);
+
+export default router;
