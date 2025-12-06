68308c47cb146301b5aab4329261669f08c51c8d
weijie <qoo77823@hotmail.com>
2025-12-06 15:29:28 +0800

    modified: app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
+import userRoutes from "./routes/user.routes";
 
 export const app = express();
 app.use(express.json());
-// app.use("/users", userRoutes);
+app.use("/users", userRoutes);
