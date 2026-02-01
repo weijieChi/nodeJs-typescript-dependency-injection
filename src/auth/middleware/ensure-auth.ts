7598b6555b95f5e434836f2f9e78a9f26aec8403
weijie <qoo77823@hotmail.com>
2026-02-01 22:09:22 +0800

    feat(auth/jwt): add jwt auth middleware to auth middleware

--- src/auth/middleware/ensure-auth.ts
+++ src/auth/middleware/ensure-auth.ts
@@ -1,16 +1,28 @@
 import type { Request, Response, NextFunction } from "express";
 import { ensureSessionAuth } from "./ensure-session-auth.js";
 import { AppError } from "../../errors/app-error.js";
+import { container } from "../../di/container.js";
+import { jwtAuth } from "../jwt/jwt.middleware.js";
 // import { ensureJwtAuth } from "./ensure-jwt-auth.js"; // 未來
 
 export async function ensureAuth(
   req: Request,
   res: Response,
-  next: NextFunction
+  next: NextFunction,
 ) {
   // import { ensureJwtAuth } from "./ensure-jwt-auth.js"; // 未來
   // Session fallback
-  if(req.cookies?.sid) {
+  const jwtAuthMiddleware = jwtAuth(container.jwtService);
+
+  const authHeader = req.headers.authorization;
+
+  // 1️⃣ 有 Bearer token → JWT
+  if (authHeader?.startsWith("Bearer")) {
+    return jwtAuthMiddleware;
+  }
+
+  // 2️⃣ 沒有 Bearer → session
+  if (req.cookies?.sid) {
     return ensureSessionAuth(req, res, next);
   }
 
